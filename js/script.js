dec9583983e85b40c7341d3c883e1bfa5cfa57f8
unknown <>
2017-01-11 10:49:04 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+$(function(){
+
+    $('.stats').append('hello electron');
+    $("#welcome").append('welcome');
+
+    console.log('欢迎');
+    var net = require('net');
+    var HOST = '192.168.1.200';
+    var PORT = 4196;
+
+    var client = new net.Socket();
+    console.log('客户端创建成功');
+    client.connect(PORT, HOST, function() {
+        console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    });
+
+    client.on('data', function(data) {
+
+        console.log('DATA: ' + data);
+        var bodyt = 2;
+        var heart = 4;
+        // 完全关闭连接
+        //client.destroy();
+    });
+
+
+
+});
---
+++ main.js
@@ -0,0 +1,38 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+const electron = require('electron');
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1000, height: 625});
+    //mainWindow.setMenu(null);//隐藏menu
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+
+    //mainWindow.openDevTools();// 打开开发工具
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
