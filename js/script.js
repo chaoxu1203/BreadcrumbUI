e49a73ef78621717a6b452f3fcbb119c548c432d
unknown <>
2017-01-13 21:03:56 +0800

    remove little tag

--- js/script.js
+++ js/script.js
@@ -1,23 +1,57 @@
 
 $(function(){
 
-    $('.stats').append('hello electron');
-
     console.log('欢迎');
     var net = require('net');
     var HOST = '192.168.1.200';
     var PORT = 4196;
 
     var client = new net.Socket();
-    console.log('客户端创建成功');
     client.connect(PORT, HOST, function() {
         console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     });
 
     client.on('data', function(data) {
+        //data = "FD 1 0100 8 56 0 26 30 75 50 50 50 FD 1 0100 3 57 1 0100 FD 1 0100 4 55 1 0 0";
+        //解析读取到的内容
         console.log('DATA: ' + data);
-        // 完全关闭连接
-        //client.destroy();
+        console.log( data.length)
+        //var gettype = Object.prototype.toString
+        //console.log(gettype.call(data) );
+
+        var datas = new Array();
+        datas = data.toString().split("\t");
+        if(data.length==35)
+        {
+            var Hex = datas[2]
+            //console.log('Hex: ' +  Hex);
+            var envorinment_temperature = datas[6]
+            //console.log('envorinment_temperature: ' +  envorinment_temperature);//30
+            var body_temperature = datas[7]
+            //console.log('body_temperature: ' +  body_temperature);
+            var heart_rate = datas[8]
+            //console.log('heart_rate: ' +  heart_rate);
+            var place_x = datas[9]
+            //console.log('place_x: ' +  place_x);
+            var place_y = datas[10]
+            //console.log('place_y: ' +  place_y);
+            var place_z = datas[11]
+            //console.log('place_z: ' +  place_z);
+        }
+        if(data.length==25)
+        {
+            var jump_step = datas[5]
+            //console.log('jump_step: ' +  jump_step);//1
+            var network_address = datas[8]
+            //console.log('network_address: ' +  network_address);//0100
+        }
+        if(data.length==21)
+        {
+            var remain_node_num = datas[6]
+            //console.log('remain_node_num: ' +  remain_node_num);//0
+            var relate_node_num = datas[7]
+            //console.log('relate_node_num: ' +  relate_node_num);//0
+        }
     });
 });
 
