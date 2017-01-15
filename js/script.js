f687697aed36b8eb6e35d1dab90a34a507768ef8
qtechbing <sa614226@mail.ustc.edu.cn>
2017-01-15 19:15:23 +0800

    更改draw

--- js/script.js
+++ js/script.js
@@ -48,31 +48,21 @@ $(function(){
     });
 });
 
-function draw(id) {
-    var canvas = document.getElementById(id);
-    if (canvas == null)
-        return false;
-    var context = canvas.getContext("2d");
-    context.fillStyle = "#EEEEFF";
-    context.fillRect(0, 0, 400, 600);
-    var n = 0;
-    var dx = 150;
-    var dy = 150;
-    var s = 100;
-    context.beginPath();
-    context.fillStyle = 'rgb(100,255,100)';
-    context.strokeStyle = 'rgb(0,0,100)';
-    var x = Math.sin(0);
-    var y = Math.cos(0);
-    var dig = Math.PI / 15 * 11;
-    for (var i = 0; i < 30; i++) {
-        var x = Math.sin(i * dig);
-        var y = Math.cos(i * dig);
-        context.lineTo(dx + x * s, dy + y * s);
+function draw(id,electricity,nodeArray) {
+    switch (id) {
+        case "line1": drawLine("line1","rgb(255,0,0)",10,10,150,150,nodeArray);
+            $('#surplusElectricity1').val(electricity);
+            break;
+        case "line2": drawLine("line2","rgb(0,255,0)",150,10,10,150,nodeArray);
+            $('#surplusElectricity2').val(electricity);
+            break;
+        case "line3": drawLine("line3","rgb(0,0,255)",150,10,10,150,nodeArray);
+            $('#surplusElectricity3').val(electricity);
+            break;
+        case "line4": drawLine("line4","rgb(255,255,0)",10,10,150,150,nodeArray);
+            $('#surplusElectricity4').val(electricity);
+            break;
     }
-    context.closePath();
-    context.fill();
-    context.stroke();
 }
 function drawLine(id,rgb,startx,starty,endx,endy,nodeArray) {
     var canvas = document.getElementById(id);
@@ -110,13 +100,4 @@ function drawLine(id,rgb,startx,starty,endx,endy,nodeArray) {
     context.lineTo(endx, endy);
     context.stroke();
     //context.fill();
-    $('#surplusElectricity1').val(30);
-    $('#surplusElectricity2').val(50);
-    $('#surplusElectricity3').val(70);
-    $('#surplusElectricity4').val(90);
 }
-
-drawLine("line1","rgb(255,0,0)",10,10,150,150,[1,2,3,4]);
-drawLine("line2","rgb(0,255,0)",150,10,10,150,[5,6,7]);
-drawLine("line3","rgb(0,0,255)",150,10,10,150,[8,9]);
-drawLine("line4","rgb(255,255,0)",10,10,150,150,[10]);
