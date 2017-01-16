3661e545d0d2a7e57b9d2ae0cba50cd40bbed838
unknown <>
2017-01-16 11:14:36 +0800

    add timeout event

--- js/script.js
+++ js/script.js
@@ -7,7 +7,17 @@ $(function(){
     var net = require('net');
     var HOST = '192.168.1.200';
     var PORT = 4196;
-    var HexSet = new Array();
+    var last_msg_time = [0, 0 ,0, 0];
+
+    setInterval(function() { //添加循环计时器
+        for(var i=0; i<last_msg_time.length; i++){
+            var cur_time = $.now();
+            if(last_msg_time[i] > 0 && cur_time - last_msg_time[i] > 10000){//超过10S，判定超时
+                cancelLine(i+1);//移除路由线
+                showWarningText(i+1);// 显示失联警告
+            }
+        }
+    }, 1000)//每秒循环一次
 
     //创建socket连接
     var client = new net.Socket();
@@ -20,70 +30,90 @@ $(function(){
         //解析读取到的内容
         console.log('DATA: ' + data);
         //console.log( data.length)
-
         var datas = new Array();
         datas = data.toString().split("\t");
-        var type = datas[4].toString
+        var type = datas[4].toString()
+        var Hex = datas[2].toString()
+        console.log("type:"+type);
+
         if(type == "56") {
             console.log('env info');
-            var Hex = datas[2]
             var envorinment_temperature = datas[6]
             var body_temperature = datas[7]
             var heart_rate = datas[8]
             var relate_node_num = datas[9]
             var remain_node_num = datas[10]
-            var battery = datas[11]
+            var idAndBattery = parseInt(datas[11]);
+            var id = idAndBattery & 240;
+            var battery = idAndBattery & 15;
 
-            var index = getIndex(Hex);
-            index = index + 1;
+            //console.log("envorinment_temperature:"+envorinment_temperature);
+            //console.log("body_temperature:"+body_temperature);
+            //console.log("heart_rate:"+heart_rate);
+            console.log("battery:"+battery);
+
+            last_msg_time[id-1] = $.now();//记录当前时间戳
 
             //将收集到的数据显示到界面上
-            $('#bodyTemperature' + index).text(body_temperature);
-            $('#heartBeat'+ index).text(heart_rate);
-            $('#temperature' + index).text(envorinment_temperature);
+            console.log("id:"+id);
+            $('#bodyTemperature' + id).text(body_temperature);
+            $('#heartBeat'+ id).text(heart_rate);
+            $('#temperature' + id).text(envorinment_temperature);
+            $('#surplusNodeNum' + id).text(remain_node_num);
+            showElectricity(id, electricity);
+
         }
         else if(type == "57") {
-            var jump_step = datas[5]
-            var network_address = datas[8]
-        }
-        else if(data.length==21) {
-            var remain_node_num = datas[6]
-            var relate_node_num = datas[7]
+            var jumpStep = parseInt(datas[5]);
+            var id = parseInt(datas[6]);
+            var route = new Array();
+            route.push(datas[2]);
+            if(jumpStep > 1){
+                route.push(datas.slice(6));
+            }
+            last_msg_time[id-1] = $.now();//记录当前时间戳
+            console.log("id:"+id);
+            console.log("jump_step:"+jumpStep);
+            console.log("route:"+route);
+            draw("line"+id, route)
         }
     });
 
-    function getIndex(Hex){
-        if(HexSet.length == 0){
-            HexSet.push(Hex);
-            return 0;
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
+
+    function showElectricity(id, battery) {
+        //var electricity = Math.floor((100.0/12) * battery);//电量满格是12
+        $('#surplusElectricity' + id).val(battery);// electricity : 1-100
+        if(battery>7){//green
         }
-        else {
-            var index = HexSet.indexOf(Hex);
-            if(index == -1){
-                HexSet.push(Hex);
-                return HexSet.length-1;
-            }
-            else{
-                return index;
-            }
+        else if(battery>3){//yellow
         }
+        else{//red
+        }
+    }
+
+
+    function showWarningText(id) {//id:1-4
+        //console.log("showWarningText:"+id);
+    }
+
+    function cancelLine(id) {
+        //console.log("cancelLine:"+id);
     }
 
 });
 
-function draw(id,electricity,nodeArray) {
+function draw(id, nodeArray) {
     switch (id) {
         case "line1": drawLine("line1","rgb(255,0,0)",10,10,150,150,nodeArray);
-            $('#surplusElectricity1').val(electricity);
             break;
         case "line2": drawLine("line2","rgb(0,255,0)",150,10,10,150,nodeArray);
-            $('#surplusElectricity2').val(electricity);
             break;
         case "line3": drawLine("line3","rgb(0,0,255)",150,10,10,150,nodeArray);
-            $('#surplusElectricity3').val(electricity);
             break;
         case "line4": drawLine("line4","rgb(255,255,0)",10,10,150,150,nodeArray);
-            $('#surplusElectricity4').val(electricity);
             break;
     }
 }
